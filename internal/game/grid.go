c589a5d075aed3b5fe70a1380ad965a9de8c11cc
BjarkeM <23573400+BjarkeM@users.noreply.github.com>
2025-11-23 20:13:53 +0100

    Allow teleporting over thickened walls, and refactor grid.go to get rid of unused teleport param

--- internal/game/grid.go
+++ internal/game/grid.go
@@ -1,6 +1,9 @@
 package game
 
-import "github.com/hectorgimenez/d2go/pkg/data"
+import (
+	"github.com/hectorgimenez/d2go/pkg/data"
+	"github.com/hectorgimenez/d2go/pkg/data/area"
+)
 
 const (
 	CollisionTypeNonWalkable CollisionType = iota
@@ -23,7 +26,7 @@ type Grid struct {
 	CollisionGrid [][]CollisionType
 }
 
-func NewGrid(rawCollisionGrid [][]CollisionType, offsetX, offsetY int, canTeleport bool, exits []data.Level) *Grid {
+func NewGrid(rawCollisionGrid [][]CollisionType, offsetX, offsetY int, exits []data.Level, areaID area.ID) *Grid {
 	grid := &Grid{
 		OffsetX:       offsetX,
 		OffsetY:       offsetY,
@@ -32,13 +35,14 @@ func NewGrid(rawCollisionGrid [][]CollisionType, offsetX, offsetY int, canTelepo
 		CollisionGrid: rawCollisionGrid,
 	}
 
-	// Let's lower the priority for the walkable tiles that are close to non-walkable tiles, so we can avoid walking too close to walls and obstacles
+	// Lower the priority for the walkable tiles that are close to non-walkable tiles, so we can avoid walking too close to walls and obstacles
+	lowPriorityRadius := 2
 	for y := 0; y < len(rawCollisionGrid); y++ {
 		for x := 0; x < len(rawCollisionGrid[y]); x++ {
 			collisionType := rawCollisionGrid[y][x]
-			if isBlockingTile(collisionType, canTeleport) {
-				for i := -2; i <= 2; i++ {
-					for j := -2; j <= 2; j++ {
+			if isBlockingTile(collisionType, false) {
+				for i := -lowPriorityRadius; i <= lowPriorityRadius; i++ {
+					for j := -lowPriorityRadius; j <= lowPriorityRadius; j++ {
 						if i == 0 && j == 0 {
 							continue
 						}
@@ -54,8 +58,8 @@ func NewGrid(rawCollisionGrid [][]CollisionType, offsetX, offsetY int, canTelepo
 		}
 	}
 
-	fillGaps(rawCollisionGrid, canTeleport)
-	thickenCollisions(rawCollisionGrid, canTeleport)
+	fillGaps(rawCollisionGrid)
+	thickenCollisions(rawCollisionGrid)
 	markDiagonalTiles(rawCollisionGrid)
 	drillExits(rawCollisionGrid, offsetX, offsetY, exits)
 
@@ -80,6 +84,9 @@ func isBlockingTile(tile CollisionType, canTeleport bool) bool {
 	}
 	if !canTeleport && tile == CollisionTypeTeleportOver {
 		return true
+	}
+	if canTeleport && tile == CollisionTypeThickened {
+		return false
 	}
 	return tile == CollisionTypeThickened
 }
@@ -128,7 +135,7 @@ func (g *Grid) Copy() *Grid {
 	}
 }
 
-func thickenCollisions(grid [][]CollisionType, canTeleport bool) {
+func thickenCollisions(grid [][]CollisionType) {
 	buffer := make([][]bool, len(grid))
 	for y := range grid {
 		buffer[y] = make([]bool, len(grid[y]))
@@ -136,7 +143,7 @@ func thickenCollisions(grid [][]CollisionType, canTeleport bool) {
 
 	for y := range grid {
 		for x := range grid[y] {
-			if !isBlockingTile(grid[y][x], canTeleport) {
+			if !isBlockingTile(grid[y][x], false) {
 				continue
 			}
 			for _, delta := range [][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} {
@@ -296,8 +303,8 @@ func drillExits(grid [][]CollisionType, offsetX, offsetY int, exits []data.Level
 	}
 }
 
-func isGap(grid [][]CollisionType, x, y int, canTeleport bool) bool {
-	if !isBlockingTile(grid[y][x], canTeleport) {
+func isGap(grid [][]CollisionType, x, y int) bool {
+	if !isBlockingTile(grid[y][x], false) {
 		return false
 	}
 
@@ -308,7 +315,7 @@ func isGap(grid [][]CollisionType, x, y int, canTeleport bool) bool {
 		if i < 0 || i >= len(grid[y]) {
 			continue
 		}
-		if isBlockingTile(grid[y][i], canTeleport) {
+		if isBlockingTile(grid[y][i], false) {
 			spaces++
 		} else {
 			spaces = 0
@@ -323,7 +330,7 @@ func isGap(grid [][]CollisionType, x, y int, canTeleport bool) bool {
 		if j < 0 || j >= len(grid) {
 			continue
 		}
-		if isBlockingTile(grid[j][x], canTeleport) {
+		if isBlockingTile(grid[j][x], false) {
 			spaces++
 		} else {
 			spaces = 0
@@ -332,11 +339,11 @@ func isGap(grid [][]CollisionType, x, y int, canTeleport bool) bool {
 	return spaces < gapSize
 }
 
-func fillGaps(grid [][]CollisionType, canTeleport bool) {
+func fillGaps(grid [][]CollisionType) {
 	for y := 0; y < len(grid); y++ {
 		row := grid[y]
 		for x := 0; x < len(row); x++ {
-			if !isGap(grid, x, y, canTeleport) {
+			if !isGap(grid, x, y) {
 				continue
 			}
 			row[x] = CollisionTypeThickened
